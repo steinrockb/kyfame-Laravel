423ac99523f12b839a86d05c4a20f7cb8a03d54b
Bethany <steinrockb@gmail.com>
2021-04-05 16:06:32 -0400

    Changed migration class names

--- database/migrations/2021_15_03_20000_create_contact_apps_table.php
+++ database/migrations/2021_15_03_20000_create_contact_apps_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactAppTable extends Migration
+class CreateContactAppsTable extends Migration
 {
     /**
      * Run the migrations.
